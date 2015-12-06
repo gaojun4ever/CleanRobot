bf62fef798cb01493fc4399f640716eca1a461b9
gaojun4ever <gaojun4ever@gmaill.com>
2015-12-06 14:42:01 +0800

    add new files

--- USER/main.c
+++ USER/main.c
@@ -1,6 +1,13 @@
 #include "stm32f4xx.h"
+#include "serial.h"
+
 
 int main()
 {
+  Serial_Init();
+  for(;;)
+  {
+		printf("hellow world\n");
+  }
 
 }
---
+++ UTILS/inc/serial.h
@@ -0,0 +1,8 @@
+#ifndef __SERIAL_H
+#define __SERIAL_H
+#include "stm32f4xx.h"
+#include "stdio.h"
+void Serial_Init(void);
+
+
+#endif
