786ba8a137174182e8769aad6a0c1d1832182363
gaojun4ever <gaojun4ever@gmaill.com>
2016-03-02 17:30:55 +0800

    modify

--- src/imu.c
+++ src/imu.c
@@ -3,14 +3,16 @@
 #include "filter.h"
 #include "protocolUtils.h"
 #include "math.h"
-// System constants
-#define deltat 0.005f // sampling period in seconds (shown as 1 ms)
-#define gyroMeasError 3.14159265358979f * (5.0f / 180.0f) // gyroscope measurement error in rad/s (shown as 5 deg/s)
-#define beta sqrt(3.0f / 4.0f) * gyroMeasError // compute beta
-// Global system variables
-// float a_x, a_y, a_z; // accelerometer measurements
-// float w_x, w_y, w_z; // gyroscope measurements in rad/s
-float SEq_1 = 1.0f, SEq_2 = 0.0f, SEq_3 = 0.0f, SEq_4 = 0.0f; // estimated orientation quaternion elements with initial conditions
+
+
+  #define BETA_DEF     0.01f    // 2 * proportional gain
+
+
+
+  float beta = BETA_DEF;     // 2 * proportional gain (Kp)
+
+
+float q0 = 1.0f, q1 = 0.0f, q2 = 0.0f, q3 = 0.0f; // estimated orientation quaternion elements with initial conditions
 
 #define GYRO_DEG_PER_LSB MPU6050_DEG_PER_LSB_2000
 #define ACCEL_G_PER_LSB MPU6050_G_PER_LSB_8
@@ -57,76 +59,99 @@ void imuUpdate(){
   GyroCal();
   AccelCal();
 
-  filterUpdate(imuStruct.Gyro.x, imuStruct.Gyro.y, imuStruct.Gyro.z, imuStruct.Accel.x, imuStruct.Accel.y, imuStruct.Accel.z);
+  filterUpdate(imuStruct.Gyro.x, imuStruct.Gyro.y, imuStruct.Gyro.z, imuStruct.Accel.x, imuStruct.Accel.y, imuStruct.Accel.z,0.005);
   GetEulerAngle();
 }
 
-
-void filterUpdate(float w_x, float w_y, float w_z, float a_x, float a_y, float a_z)
+//---------------------------------------------------------------------------------------------------
+// Fast inverse square-root
+// See: http://en.wikipedia.org/wiki/Fast_inverse_square_root
+float invSqrt(float x)
 {
-  // Local system variables
-  float norm; // vector norm
-  float SEqDot_omega_1, SEqDot_omega_2, SEqDot_omega_3, SEqDot_omega_4; // quaternion derrivative from gyroscopes elements
-  float f_1, f_2, f_3; // objective function elements
-  float J_11or24, J_12or23, J_13or22, J_14or21, J_32, J_33; // objective function Jacobian elements
-  float SEqHatDot_1, SEqHatDot_2, SEqHatDot_3, SEqHatDot_4; // estimated direction of the gyroscope error
-  // Axulirary variables to avoid reapeated calcualtions
-  float halfSEq_1 = 0.5f * SEq_1;
-  float halfSEq_2 = 0.5f * SEq_2;
-  float halfSEq_3 = 0.5f * SEq_3;
-  float halfSEq_4 = 0.5f * SEq_4;
-  float twoSEq_1 = 2.0f * SEq_1;
-  float twoSEq_2 = 2.0f * SEq_2;
-  float twoSEq_3 = 2.0f * SEq_3;
-  // Normalise the accelerometer measurement
-  norm = sqrt(a_x * a_x + a_y * a_y + a_z * a_z);
-  a_x /= norm;
-  a_y /= norm;
-  a_z /= norm;
-  // Compute the objective function and Jacobian
-  f_1 = twoSEq_2 * SEq_4 - twoSEq_1 * SEq_3 - a_x;
-  f_2 = twoSEq_1 * SEq_2 + twoSEq_3 * SEq_4 - a_y;
-  f_3 = 1.0f - twoSEq_2 * SEq_2 - twoSEq_3 * SEq_3 - a_z;
-  J_11or24 = twoSEq_3; // J_11 negated in matrix multiplication
-  J_12or23 = 2.0f * SEq_4;
-  J_13or22 = twoSEq_1; // J_12 negated in matrix multiplication
-  J_14or21 = twoSEq_2;
-  J_32 = 2.0f * J_14or21; // negated in matrix multiplication
-  J_33 = 2.0f * J_11or24; // negated in matrix multiplication
-  // Compute the gradient (matrix multiplication)
-  SEqHatDot_1 = J_14or21 * f_2 - J_11or24 * f_1;
-  SEqHatDot_2 = J_12or23 * f_1 + J_13or22 * f_2 - J_32 * f_3;
-  SEqHatDot_3 = J_12or23 * f_2 - J_33 * f_3 - J_13or22 * f_1;
-  SEqHatDot_4 = J_14or21 * f_1 + J_11or24 * f_2;
-  // Normalise the gradient
-  norm = sqrt(SEqHatDot_1 * SEqHatDot_1 + SEqHatDot_2 * SEqHatDot_2 + SEqHatDot_3 * SEqHatDot_3 + SEqHatDot_4 * SEqHatDot_4);
-  SEqHatDot_1 /= norm;
-  SEqHatDot_2 /= norm;
-  SEqHatDot_3 /= norm;
-  SEqHatDot_4 /= norm;
-  // Compute the quaternion derrivative measured by gyroscopes
-  SEqDot_omega_1 = -halfSEq_2 * w_x - halfSEq_3 * w_y - halfSEq_4 * w_z;
-  SEqDot_omega_2 = halfSEq_1 * w_x + halfSEq_3 * w_z - halfSEq_4 * w_y;
-  SEqDot_omega_3 = halfSEq_1 * w_y - halfSEq_2 * w_z + halfSEq_4 * w_x;
-  SEqDot_omega_4 = halfSEq_1 * w_z + halfSEq_2 * w_y - halfSEq_3 * w_x;
-  // Compute then integrate the estimated quaternion derrivative
-  SEq_1 += (SEqDot_omega_1 - (beta * SEqHatDot_1)) * deltat;
-  SEq_2 += (SEqDot_omega_2 - (beta * SEqHatDot_2)) * deltat;
-  SEq_3 += (SEqDot_omega_3 - (beta * SEqHatDot_3)) * deltat;
-  SEq_4 += (SEqDot_omega_4 - (beta * SEqHatDot_4)) * deltat;
+  float halfx = 0.5f * x;
+  float y = x;
+  long i = *(long*)&y;
+  i = 0x5f3759df - (i>>1);
+  y = *(float*)&i;
+  y = y * (1.5f - (halfx * y * y));
+  return y;
+}
+
+
+void filterUpdate(float gx, float gy, float gz, float ax, float ay, float az, float dt)
+{
+  float recipNorm;
+  float s0, s1, s2, s3;
+  float qDot1, qDot2, qDot3, qDot4;
+  float _2q0, _2q1, _2q2, _2q3, _4q0, _4q1, _4q2 ,_8q1, _8q2, q0q0, q1q1, q2q2, q3q3;
+
+  // Rate of change of quaternion from gyroscope
+  qDot1 = 0.5f * (-q1 * gx - q2 * gy - q3 * gz);
+  qDot2 = 0.5f * (q0 * gx + q2 * gz - q3 * gy);
+  qDot3 = 0.5f * (q0 * gy - q1 * gz + q3 * gx);
+  qDot4 = 0.5f * (q0 * gz + q1 * gy - q2 * gx);
+
+  // Compute feedback only if accelerometer measurement valid (avoids NaN in accelerometer normalisation)
+  if(!((ax == 0.0f) && (ay == 0.0f) && (az == 0.0f)))
+  {
+   // Normalise accelerometer measurement
+   recipNorm = invSqrt(ax * ax + ay * ay + az * az);
+   ax *= recipNorm;
+   ay *= recipNorm;
+   az *= recipNorm;
+
+   // Auxiliary variables to avoid repeated arithmetic
+   _2q0 = 2.0f * q0;
+   _2q1 = 2.0f * q1;
+   _2q2 = 2.0f * q2;
+   _2q3 = 2.0f * q3;
+   _4q0 = 4.0f * q0;
+   _4q1 = 4.0f * q1;
+   _4q2 = 4.0f * q2;
+   _8q1 = 8.0f * q1;
+   _8q2 = 8.0f * q2;
+   q0q0 = q0 * q0;
+   q1q1 = q1 * q1;
+   q2q2 = q2 * q2;
+   q3q3 = q3 * q3;
+
+   // Gradient decent algorithm corrective step
+   s0 = _4q0 * q2q2 + _2q2 * ax + _4q0 * q1q1 - _2q1 * ay;
+   s1 = _4q1 * q3q3 - _2q3 * ax + 4.0f * q0q0 * q1 - _2q0 * ay - _4q1 + _8q1 * q1q1 + _8q1 * q2q2 + _4q1 * az;
+   s2 = 4.0f * q0q0 * q2 + _2q0 * ax + _4q2 * q3q3 - _2q3 * ay - _4q2 + _8q2 * q1q1 + _8q2 * q2q2 + _4q2 * az;
+   s3 = 4.0f * q1q1 * q3 - _2q1 * ax + 4.0f * q2q2 * q3 - _2q2 * ay;
+   recipNorm = invSqrt(s0 * s0 + s1 * s1 + s2 * s2 + s3 * s3); // normalise step magnitude
+   s0 *= recipNorm;
+   s1 *= recipNorm;
+   s2 *= recipNorm;
+   s3 *= recipNorm;
+
+   // Apply feedback step
+   qDot1 -= beta * s0;
+   qDot2 -= beta * s1;
+   qDot3 -= beta * s2;
+   qDot4 -= beta * s3;
+  }
+
+  // Integrate rate of change of quaternion to yield quaternion
+  q0 += qDot1 * dt;
+  q1 += qDot2 * dt;
+  q2 += qDot3 * dt;
+  q3 += qDot4 * dt;
+
   // Normalise quaternion
-  norm = sqrt(SEq_1 * SEq_1 + SEq_2 * SEq_2 + SEq_3 * SEq_3 + SEq_4 * SEq_4);
-  SEq_1 /= norm;
-  SEq_2 /= norm;
-  SEq_3 /= norm;
-  SEq_4 /= norm;
+  recipNorm = invSqrt(q0*q0 + q1*q1 + q2*q2 + q3*q3);
+  q0 *= recipNorm;
+  q1 *= recipNorm;
+  q2 *= recipNorm;
+  q3 *= recipNorm;
 }
 
 void GetEulerAngle(void)
 {
-  imuStruct.eulerAngle.x=atan2(2*(SEq_1*SEq_2+SEq_3*SEq_4),(1-2*(SEq_2*SEq_2+SEq_3*SEq_3)))*57.3;
-  imuStruct.eulerAngle.y=asin(2*(SEq_1*SEq_3-SEq_2*SEq_4))*57.3;
-  imuStruct.eulerAngle.z=atan2(2*(SEq_1*SEq_4+SEq_2*SEq_3),(1-2*(SEq_4*SEq_4+SEq_3*SEq_3)))*57.3;
+  imuStruct.eulerAngle.x=atan2(2*(q0*q1+q2*q3),(1-2*(q1*q1+q2*q2)))*57.3;
+  imuStruct.eulerAngle.y=asin(2*(q0*q2-q1*q3))*57.3;
+  imuStruct.eulerAngle.z=atan2(2*(q0*q3+q1*q2),(1-2*(q3*q3+q2*q2)))*57.3;
   printf("%f,%f,%f\n",imuStruct.eulerAngle.x,imuStruct.eulerAngle.y,imuStruct.eulerAngle.z );
   //sendDataProtocol(&imuStruct.eulerAngle);
 }
