17f5157fff69c3d7bbaef33936991d7c7f6f2b19
gaojun4ever <gaojun4ever@gmaill.com>
2016-03-04 17:00:14 +0800

    delete

--- inc/filter.h
+++
@@ -1,3 +0,0 @@
-#include "config.h"
-#define SHIFT_NUM 8
-int16_t iirLPF(int16_t in);
--- inc/protocolUtils.h
+++
@@ -1,4 +0,0 @@
-#include "config.h"
-#include "imu.h"
-void addProtocolCmd(Axis3_Float_Typedef* angle);
-void sendDataProtocol(Axis3_Float_Typedef* angle);
